eae203e824f371cce0b1eaa98fb4b3603f317854
joagranadosme <joagranadosme@unal.edu.co>
2018-06-04 22:56:02 -0500

    Sun light working!

--- app/src/main/java/processing/test/toonshading/Sketch.java
+++ app/src/main/java/processing/test/toonshading/Sketch.java
@@ -7,6 +7,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import frames.primitives.Frame;
+import frames.primitives.Quaternion;
+import frames.primitives.Vector;
+import frames.processing.Scene;
 import processing.core.*;
 import processing.data.*; 
 import processing.event.*; 
@@ -47,8 +50,10 @@ public class Sketch extends PApplet {
     private Frame universe, sun, earth, moon;
 
     //Shaders variables
-    PShader toon;
-    boolean shaderEnabled = false;
+    PImage sunImage, earthImage, moonImage;
+    PShape sunShape, earthShape, moonShape;
+    PShader light;
+    int r;
 
     public void settings() {
         fullScreen(P3D);
@@ -58,27 +63,115 @@ public class Sketch extends PApplet {
 
         orientation(LANDSCAPE);
 
+        background(0);
         noStroke();
-        fill(204);
-        toon = loadShader("ToonFrag.glsl", "ToonVert.glsl");
+
+        context = getActivity(); //Get context
+
+        //Initialize sensor
+        manager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        listener = new AccelerometerListener();
+        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME);
+        textFont(createFont("SansSerif", 10 * displayDensity));
+
+        //Initialize shader
+        light = loadShader("texlightfrag.glsl", "texlightvert.glsl");
+
+        //Initialize universe
+        universe = new Frame();
+        universe.translate(width/2, height/2);
+
+        //Initialize sun
+        sun = new Frame(universe, new Vector(0,0), new Quaternion());
+        sunImage = loadImage( "sun.jpg");
+        sunShape = createShape(SPHERE, 100);
+        sunShape.setTexture(sunImage);
+
+        //Initialize earth
+        earth = new Frame(sun, new Vector(400, 0), new Quaternion());
+        earthImage = loadImage( "world.jpg");
+        earthShape = createShape(SPHERE, 50);
+        earthShape.setTexture(earthImage);
+
+        //Initialize moon
+        moon = new Frame(earth, new Vector(100, 0), new Quaternion());
+        moonImage = loadImage( "moon.jpg");
+        moonShape = createShape(SPHERE, 10);
+        moonShape.setTexture(moonImage);
+
+
 
     }
 
     public void draw() {
 
-        if (shaderEnabled) {
-            shader(toon);
-        }
+        //Set Background
+        background(0);
+
+        //Call shader
+        shader(light);
+        //Point light
+        pointLight(255, 255, 255, width/2, height/2, 0);
+        //lightFalloff(width/2, height/2, 0.00001F);
+        //ambientLight(255, 255, 255, width/2, height/2, 0);
+
+        //Display sensor values
+        text("X: " + ax + "\nY: " + ay + "\nZ: " + az, 0, 0, width, height);
+
+        //Update all frames
+        updateFrames();
 
         noStroke();
-        background(0);
-        float dirY = (mouseY / PApplet.parseFloat(height) - 0.5f) * 2;
-        float dirX = (mouseX / PApplet.parseFloat(width) - 0.5f) * 2;
-        directionalLight(204, 204, 204, -dirX, -dirY, -1);
-        translate(width/2, height/2);
-        sphere(120);
+
+        //Draw all frames
+        push();
+        Scene.applyTransformation(this.g, universe);
+        push();
+        Scene.applyTransformation(this.g, sun);
+        pointLight(255, 255, 255, 0, 0, 0);
+        push();
+        Scene.applyTransformation(this.g, earth);
+        shape(earthShape);
+        push();
+        Scene.applyTransformation(this.g, moon);
+        shape(moonShape);
+        pop();
+        pop();
+        resetShader();
+        ambientLight(255, 255, 255);
+        shape(sunShape);
+        pop();
+        pop();
 
     }
+
+    public void push() {
+        pushStyle();
+        pushMatrix();
+    }
+
+    public void pop() {
+        popStyle();
+        popMatrix();
+    }
+
+    public void planet(float r) {
+        sphere(r);
+    }
+
+    public void updateFrames() {
+
+        //if(flag)
+            rotation++;
+
+        universe.setRotation(new Quaternion(new Vector(1.0F, 0.0F, 0.0F), radians(-20F * ax)));
+        sun.setRotation(new Quaternion(new Vector(0.0F, 1.0F, 0.0F), radians(0.5F * rotation)));
+        earth.setRotation(new Quaternion(new Vector(0.0F, 1.0F, 0.0F), radians(2*rotation)));
+        moon.setRotation(new Quaternion(new Vector(0.0F, 1.0F, 0.0F), radians(4*rotation)));
+
+    }
+
 
     //Class for receive the events from Accelerometer
     class AccelerometerListener implements SensorEventListener {
